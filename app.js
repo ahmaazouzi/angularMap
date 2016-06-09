b016a3ef6b540a42a4ef66f2669b351d6867f1f5
Ahmed Maazouzi <ahmaazouzi@mix.wvu.edu>
2016-06-09 02:53:31 -0400

    added styling

--- app.js
+++ app.js
@@ -38,17 +38,25 @@ var cities = [
 
 ];
 
-var app1 = angular.module('app1', ['ui.grid']);
+var app1 = angular.module('app1', []);
 app1.controller('mappa', function($scope){
+
+    var styles = [{"elementType":"geometry","stylers":[{"hue":"#ff4400"},{"saturation":-68},{"lightness":-4},{"gamma":0.72}]},{"featureType":"road","elementType":"labels.icon"},{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"hue":"#0077ff"},{"gamma":3.1}]},{"featureType":"water","stylers":[{"hue":"#00ccff"},{"gamma":0.44},{"saturation":-33}]},{"featureType":"poi.park","stylers":[{"hue":"#44ff00"},{"saturation":-23}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"hue":"#007fff"},{"gamma":0.77},{"saturation":65},{"lightness":99}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"gamma":0.11},{"weight":5.6},{"saturation":99},{"hue":"#0091ff"},{"lightness":-86}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"lightness":-48},{"hue":"#ff5e00"},{"gamma":1.2},{"saturation":-23}]},{"featureType":"transit","elementType":"labels.text.stroke","stylers":[{"saturation":-64},{"hue":"#ff9100"},{"lightness":16},{"gamma":0.47},{"weight":2.7}]}]
+
+
     var mapOptions = {
           center: {lat: 39.0119, lng: -98.4842},
           zoom: 5,
         mapTypeControlOptions: {
             position: google.maps.ControlPosition.TOP_RIGHT}      
       };
+
     $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    $scope.map.setOptions({styles: styles});
     $scope.markers = [];
+
     $scope.myData = JSON.stringify(cities);
+
     var infoWindow = new google.maps.InfoWindow({maxWidth:200});
 
     var Markado = function(argumado){
@@ -60,7 +68,6 @@ app1.controller('mappa', function($scope){
 
         marker.content = '<div>' + argumado.desc + '</div>'
         marker.nana = argumado.city
-
 
         $scope.markers.push(marker);
         marker.addListener('click', function() {
@@ -83,12 +90,15 @@ app1.controller('mappa', function($scope){
 
 });
 
-// $("button").click(function() {
-//   $(".col-md-4").slideToggle();
-// });
+
 var duration = 'slow';
 $("button").click(function() {
   $(".col-md-4").animate({width:'toggle'}, 150);
 });
-// 'slide', 
+
+$("i").click(function() {
+  $(".col-md-4").animate({width:'toggle'}, 150);
+});
+
+
 
